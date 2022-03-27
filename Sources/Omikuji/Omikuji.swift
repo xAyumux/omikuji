f88fd7e5decca13ea28cd75d5533713d20ebc804
xAyumux <ayumu.f.1127@gmail.com>
2022-03-27 17:54:10 +0900

    first commit

--- Sources/Omikuji/Omikuji.swift
+++ Sources/Omikuji/Omikuji.swift
@@ -1,6 +1,27 @@
-public struct Omikuji {
-    public private(set) var text = "Hello, World!"
+//public struct Omikuji {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
 
-    public init() {
+public enum Omikuji: String, CaseIterable {
+    case excellentLuck
+    case veryGoodLuck
+    case goodLuck
+    case slightlyGoodLuck
+    case uncertainLuck
+    case badLuck
+}
+
+extension Omikuji {
+    public static func draw() -> Self {
+        allCases.randomElement()!
+    }
+}
+
+extension Omikuji: CustomStringConvertible {
+    public var description: String {
+        rawValue
     }
 }
